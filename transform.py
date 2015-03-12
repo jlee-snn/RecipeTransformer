f852a66ac65245c409cb070efa72ceb038555697
Alex Kowalczuk <askowalczuk93@gmail.com>
2015-03-11 21:00:52 -0500

    distant and matching functions

--- transform.py
+++ transform.py
@@ -1,6 +1,7 @@
 from parser.RecipeParse import RecipeParse
 import json
 from random import randrange
+import math
 
 NON_VEGETARIAN_GROUPS = [
     "Sausages and Luncheon Meats",
@@ -8,7 +9,8 @@ NON_VEGETARIAN_GROUPS = [
     "Beef Products",
     "Finfish and Shellfish Products",
     "Lamb, Veal, and Game Products",
-    "Poultry Products"
+    "Poultry Products",
+    "Ethnic Foods"
 ]
 
 NON_PESCETARIAN_GROUPS = [
@@ -16,7 +18,8 @@ NON_PESCETARIAN_GROUPS = [
     "Pork Products",
     "Beef Products",
     "Lamb, Veal, and Game Products",
-    "Poultry Products"
+    "Poultry Products",
+    "Ethnic Foods"
 ]
 
 def filter_food_groups(url, groups):
@@ -34,12 +37,13 @@ def filter_food_groups(url, groups):
 
     for ingredient in recipe["ingredients"]:
         nut_info = resolve_ingredient(ingredient["name"], food_data)
-        if nut_info["group"] in groups:
-            print "Attempting to replace ", nut_info["description"]
+        if nut_info[u'group'] in groups:
+            print "Attempting to replace:", nut_info[u'description']
+            print nut_info[u'group']
             replacement = find_similar_food(nut_info, groups, food_data)
-            print "Replacing with", replacement["description"]
-            subs[ingredient["name"]] = replacement["description"]
-
+            print "Replacing with:", replacement[u'description']
+            print replacement[u'group']
+            subs[ingredient["name"]] = replacement[u'description']
     for ingredient in recipe["ingredients"]:
         if ingredient["name"] in subs:
             ingredient["name"] = subs[ingredient["name"]]
@@ -57,17 +61,66 @@ def resolve_ingredient(name, data):
     Takes an ingredient name (as given by our allrecipies parser), and returns a
     dict from the FDA database with the closest match for this ingredient.
     """
-
-    # obviously broken
-    return data[randrange(0,6000)]
+    name_tokens = name.lower().replace(',','').split()
+    best_match = {}
+    best_match_tokens = []
+    matching_words = 0
+    for ingredient in data:
+    	ingredient_tokens = ingredient[u'description'].lower().replace(',','').split()
+    	matches = [itm for itm in ingredient_tokens if itm in name_tokens]
+    	"""
+    	Figured that the best match would be the one with the shortest ingredient name length if there is
+    	a conflict where two ingredients have the same number of matches.
+    	"""
+    	if len(matches) == matching_words:
+    		if len(ingredient_tokens) < len(best_match_tokens):
+    			best_match = ingredient
+    			best_match_tokens = ingredient_tokens
+    	if len(matches) > matching_words:
+    		matching_words = len(matches)
+    		best_match = ingredient
+    		best_match_tokens = ingredient_tokens
+    return best_match
 
 def find_similar_food(ingredient, groups, data):
     """
     Takes an ingredient (the whole dict) and a list of banned groups, and finds
     a similar ingredient in the food database
     """
+    best_distance = 2000
+    best_match = {}
+    for item in data:
+    	if item[u'group'] not in groups:
+    		distance = calculate_distance(ingredient, item)
+    		if distance < best_distance:
+    			best_distance = distance
+    			best_match = item
+    print best_distance
+    return best_match
 
-    # obviously broken
-    return data[randrange(0,6000)]
+def calculate_distance(ingredient, arg_ingredient):
+	"""
+	Each ingredient has different numbers of nutrients, so the distance is calculated
+	by only using nutrients present in both ingredients (of the first 5 nutrients).
+	"""
+	distance = 0.0
+	ingredient_nutrients = {}
+	arg_ingredient_nutrients = {}
+	for i in range(0, 5):
+		ingredient_nutrients[ingredient[u'nutrients'][i][u'description']] = ingredient[u'nutrients'][i][u'value']
+	for i in range(0, 5):
+		arg_ingredient_nutrients[arg_ingredient[u'nutrients'][i][u'description']] = arg_ingredient[u'nutrients'][i][u'value']
+	for nutrient in ingredient_nutrients.keys():
+		count = 0
+		if nutrient in arg_ingredient_nutrients.keys():
+			count = float(ingredient_nutrients[nutrient]) - float(arg_ingredient_nutrients[nutrient])
+			count = count * count
+			distance = distance + count
+	distance = 	float(math.sqrt(distance))
+	return distance
 
-print to_vegetarian("http://allrecipes.com/Recipe/Asian-Beef-with-Snow-Peas/Detail.aspx?soid=recs_recipe_2")
+def pretty_print_dict(dict):
+	print json.dumps(dict, indent = 4)
+	return
+	
+to_vegetarian("http://allrecipes.com/Recipe/Denises-Peanut-Chicken/Detail.aspx?prop24=hn_slide1_Denise%27s-Peanut-Chicken&evt19=1")
